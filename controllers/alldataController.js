f86b8ece8743399b67cba883276c84fc732fd0ef
Swaroopkarneddi <swaroopweb07@gmail.com>
2025-03-21 00:00:01 +0530

    data

--- controllers/alldataController.js
+++ controllers/alldataController.js
@@ -6,8 +6,8 @@ const getAllData = async (req, res) => {
 
   try {
     const [leetcodeContests, codeforcesRatingHistory] = await Promise.all([
-      getAttendedLeetcodeContestData(leetcodeId), // Pass username directly
-      getCodeforcesRatingHistory(codeforcesId), // Pass username directly
+      getAttendedLeetcodeContestData(leetcodeId),
+      getCodeforcesRatingHistory(codeforcesId),
     ]);
 
     res.json({
@@ -19,4 +19,45 @@ const getAllData = async (req, res) => {
   }
 };
 
-module.exports = { getAllData };
+const getOverallRanking = async (req, res) => {
+  const { leetcodeId, codeforcesId } = req.params;
+
+  try {
+    const [leetcodeContests, codeforcesRatingHistory] = await Promise.all([
+      getAttendedLeetcodeContestData(leetcodeId),
+      getCodeforcesRatingHistory(codeforcesId),
+    ]);
+
+    let totalRanking = 0;
+    let totalRating = 0;
+    let count = 0;
+
+    // Process LeetCode rankings and ratings
+    leetcodeContests.forEach((contest) => {
+      totalRanking += contest.ranking;
+      totalRating += contest.rating;
+      count++;
+    });
+
+    // Process Codeforces rankings and ratings
+    codeforcesRatingHistory.forEach((contest) => {
+      totalRanking += contest.rank;
+      totalRating += contest.newRating;
+      count++;
+    });
+
+    const averageRanking = count > 0 ? totalRanking / count : 0;
+    const averageRating = count > 0 ? totalRating / count : 0;
+
+    res.json({
+      leetcodeAttendedContests: leetcodeContests,
+      codeforcesRatingHistory: codeforcesRatingHistory,
+      overallRanking: Math.round(averageRanking),
+      overallRating: Math.round(averageRating),
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllData, getOverallRanking };
--- routes/alldataRoutes.js
+++ routes/alldataRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getAllData } = require("../controllers/alldataController");
+const {
+  getAllData,
+  getOverallRanking,
+} = require("../controllers/alldataController");
 
 const router = express.Router();
 
 router.get("/:leetcodeId/:codeforcesId", getAllData);
+router.get("/ov/:leetcodeId/:codeforcesId", getOverallRanking);
 
 module.exports = router;
